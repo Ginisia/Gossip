489cc06841c68843b2b4edc7ca8d74b68df75b19
Gini <djinymail@yahoo.fr>
2022-01-16 17:34:53 +0100

    Last Update

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,2 +1,11 @@
-console.log('hey')
-console.log('hey')
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	autoplay: {
+		delay: 3000,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
